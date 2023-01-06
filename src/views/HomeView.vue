a8092abfee81aa962c049f96ea430c08b3bc1798
luozhanxin <luozhanxin27@gmail.com>
2023-01-06 16:05:48 +0800

    create inital setup for monk project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <div class="main">
+      <h1>This is an main page</h1>
+      <hello msg="hello"></hello>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Hello from "../components/Hello";
+export default defineComponent({
+  components: {
+    Hello,
+  },
+});
+</script>
