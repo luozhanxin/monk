9ee005ec47b2bf7cc890cd467c975b3b02f114f2
luozhanxin <luozhanxin27@gmail.com>
2023-01-13 09:34:09 +0800

    add upload slot and test

--- src/components/UploaderFile.vue
+++ src/components/UploaderFile.vue
@@ -1,66 +1,208 @@
 <template>
   <div class="file-upload">
-    <button @click="triggerUpload">
-      <span v-if="fileStatus === 'loading'">正在上传</span>
-      <span v-else-if="fileStatus === 'success'">上传成功</span>
-      <span v-else-if="fileStatus === 'error'">上传失败</span>
-      <span v-else>点击上传</span>
-    </button>
+    <div class="upload-area" @click="triggerUpload" :disabled="isUploading">
+      <slot v-if="isUploading" name="loading"
+        ><button disabled>正在上传</button></slot
+      >
+      <slot
+        name="uploaded"
+        v-else-if="lastFileData && lastFileData.loaded"
+        :uploadedData="lastFileData.data"
+      >
+        <button>点击上传</button>
+      </slot>
+      <slot v-else name="default"><button>点击上传</button></slot>
+    </div>
     <input
       ref="fileInput"
       type="file"
       :style="{ display: 'none' }"
       @change="handleFileChange"
     />
+    <ul class="upload-list">
+      <li
+        :class="`uploaded-file upload-${file.status}`"
+        v-for="file in uploadedFiles"
+        :key="file.uid"
+      >
+        <span v-if="file.status === 'loading'"><LoadingOutlined /></span>
+        <span v-else class="file-icon"><FileOutlined /></span>
+        <span class="filename">{{ file.name }}</span>
+        <button class="delete-icon" @click="removeFile(file.uid)">
+          <DeleteOutlined />
+        </button>
+      </li>
+    </ul>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, reactive, computed, type PropType } from "vue";
+import {
+  DeleteOutlined,
+  LoadingOutlined,
+  FileOutlined,
+} from "@ant-design/icons-vue";
 import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import { last } from "lodash-es";
 type UploadStatus = "ready" | "loading" | "success" | "error";
+type CheckUpload = (file: File) => boolean | Promise<File>;
+export interface UploadFile {
+  uid: string;
+  size: number;
+  name: string;
+  status: UploadStatus;
+  raw: File;
+  resp?: any;
+}
 export default defineComponent({
   props: {
     action: {
       type: String,
       required: true,
     },
+    beforeUpload: {
+      type: Function as PropType<CheckUpload>,
+    },
+    drag: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  components: {
+    DeleteOutlined,
+    LoadingOutlined,
+    FileOutlined,
   },
   setup(props) {
     const fileInput = ref<null | HTMLInputElement>(null);
-    const fileStatus = ref<UploadStatus>("ready");
+    const uploadedFiles = ref<UploadFile[]>([]);
+    const isUploading = computed(() => {
+      return uploadedFiles.value.some((file) => file.status === "loading");
+    });
+    const lastFileData = computed(() => {
+      const lastFile = last(uploadedFiles.value);
+      if (lastFile) {
+        return {
+          loaded: lastFile.status === "success",
+          data: lastFile.resp,
+        };
+      }
+      return false;
+    });
+    const removeFile = (id: string) => {
+      uploadedFiles.value = uploadedFiles.value.filter(
+        (file) => file.uid !== id
+      );
+    };
     const triggerUpload = () => {
       if (fileInput.value) {
         fileInput.value.click();
       }
+    };
+    const postFile = (uploadedFile: File) => {
+      const formData = new FormData();
+      formData.append(uploadedFile.name, uploadedFile);
+      const fileObj = reactive<UploadFile>({
+        uid: uuidv4(),
+        size: uploadedFile.size,
+        name: uploadedFile.name,
+        status: "loading",
+        raw: uploadedFile,
+      });
+      uploadedFiles.value.push(fileObj);
+      axios
+        .post(props.action, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((resp) => {
+          fileObj.status = "success";
+          fileObj.resp = resp.data;
+        })
+        .catch(() => {
+          fileObj.status = "error";
+        })
+        .finally(() => {
+          if (fileInput.value) {
+            fileInput.value.value = "";
+          }
+        });
     };
     const handleFileChange = (e: Event) => {
       const target = e.target as HTMLInputElement;
       const files = target.files;
       if (files) {
         const uploadedFile = files[0];
-        const formData = new FormData();
-        formData.append(uploadedFile.name, uploadedFile);
-        fileStatus.value = "loading";
-        axios
-          .post(props.action, formData, {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          })
-          .then((resp) => {
-            fileStatus.value = "success";
-          })
-          .catch(() => {
-            fileStatus.value = "error";
-          });
+        if (props.beforeUpload) {
+          const result = props.beforeUpload(uploadedFile);
+          if (result && result instanceof Promise) {
+            result
+              .then((processedFile) => {
+                if (processedFile instanceof File) {
+                  postFile(processedFile);
+                } else {
+                  throw new Error(
+                    "beforeUpload Promise should return File object"
+                  );
+                }
+              })
+              .catch((e) => {
+                console.log(e);
+              });
+          } else if (result === true) {
+            postFile(uploadedFile);
+          }
+        } else {
+          postFile(uploadedFile);
+        }
       }
     };
     return {
       fileInput,
-      fileStatus,
+      isUploading,
+      uploadedFiles,
+      lastFileData,
       triggerUpload,
+      removeFile,
       handleFileChange,
     };
   },
 });
 </script>
+
+<style scoped>
+.upload-loading {
+  color: yellow;
+}
+
+.upload-success {
+  color: green;
+}
+
+.upload-error {
+  color: red;
+}
+
+.upload-list {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+.upload-list li {
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+  font-size: 14px;
+  line-height: 1.8;
+  margin-top: 5px;
+  box-sizing: border-box;
+  border-radius: 4px;
+  min-width: 200px;
+  position: relative;
+}
+
+.upload-list > li:first-child {
+  margin-top: 10px;
+}
+</style>
--- src/tests/unit/UploaderFile.spec.ts
+++ src/tests/unit/UploaderFile.spec.ts
@@ -1,5 +1,10 @@
-import { shallowMount, VueWrapper, flushPromises } from "@vue/test-utils";
-import { describe, it, expect, beforeAll, vi } from "vitest";
+import {
+  shallowMount,
+  VueWrapper,
+  flushPromises,
+  mount,
+} from "@vue/test-utils";
+import { describe, it, expect, beforeAll, vi, afterEach } from "vitest";
 import UploaderFile from "@/components/UploaderFile.vue";
 import axios from "axios";
 
@@ -7,6 +12,23 @@ const mockAxios = axios as vi.importActual<typeof axios>;
 let wrapper: VueWrapper<any>;
 const testFile = new File(["xyz"], "test.png", { type: "image/png" });
 vi.mock("axios");
+
+const mockComponent = {
+  template: "<div><slot></slot></div>",
+};
+const mockComponents = {
+  DeleteOutlined: mockComponent,
+  LoadingOutlined: mockComponent,
+  FileOutlined: mockComponent,
+};
+const setInputValue = (input: HTMLInputElement) => {
+  const files = [testFile] as any;
+  // hack
+  Object.defineProperty(input, "files", {
+    value: files,
+    writable: false,
+  });
+};
 
 describe("UploaderFile Component", () => {
   beforeAll(() => {
@@ -19,31 +41,27 @@ describe("UploaderFile Component", () => {
 
   it("basic layout before upload", () => {
     expect(wrapper.find("button").exists()).toBeTruthy();
-    expect(wrapper.get("button span").text()).toBe("点击上传");
+    expect(wrapper.get("button").text()).toBe("点击上传");
     expect(wrapper.get("input").isVisible()).toBeFalsy();
   });
   it("upload process should works fine", async () => {
     // change e.target.files
     mockAxios.post.mockResolvedValueOnce({ status: "success" });
     const fileInput = wrapper.find("input").element as HTMLInputElement;
-    const files = [testFile] as any;
-    // hack
-    Object.defineProperty(fileInput, "files", {
-      value: files,
-      writable: false,
-    });
+    setInputValue(fileInput);
+
     // create a file
     await wrapper.get("input").trigger("change");
     expect(mockAxios.post).toHaveBeenCalledTimes(1);
-    expect(wrapper.get("button span").text()).toBe("正在上传");
+    expect(wrapper.get("button").text()).toBe("正在上传");
     // button 为 disabled
-    expect(wrapper.get("button").attributes("disabled")).toBeTruthy();
+    expect(wrapper.get("button").attributes()).toHaveProperty("disabled");
     // 列表长度修改，并且有正确的 class
     expect(wrapper.findAll("li").length).toBe(1);
     const firstItem = wrapper.get("li:first-child");
-    expect(firstItem.classes()).toContain("uploading");
+    expect(firstItem.classes()).toContain("upload-loading");
     await flushPromises();
-    expect(wrapper.get("button span").text()).toBe("上传成功");
+    expect(wrapper.get("button").text()).toBe("点击上传");
     // 有正确的 class，并且文件名称相对应
     expect(firstItem.classes()).toContain("upload-success");
     expect(firstItem.get(".filename").text()).toBe(testFile.name);
@@ -51,9 +69,111 @@ describe("UploaderFile Component", () => {
   it("should return error text when post is rejected", async () => {
     mockAxios.post.mockRejectedValueOnce({ error: "error" });
     await wrapper.get("input").trigger("change");
-    expect(mockAxios.post).toHaveBeenCalledTimes(2);
-    expect(wrapper.get("button span").text()).toBe("正在上传");
+    expect(mockAxios.post).toHaveBeenCalledTimes(1);
+    expect(wrapper.get("button").text()).toBe("正在上传");
     await flushPromises();
-    expect(wrapper.get("button span").text()).toBe("上传失败");
+    expect(wrapper.get("button").text()).toBe("点击上传");
+    // 列表长度增加，并且列表最后一项有正确的 class 名
+    expect(wrapper.findAll("li").length).toBe(2);
+    const lastItem = wrapper.get("li:last-child");
+    expect(lastItem.classes()).toContain("upload-error");
+    await lastItem.get(".delete-icon").trigger("click");
+    expect(wrapper.findAll("li").length).toBe(1);
+  });
+  it("should show the correct interface when using custom slot", async () => {
+    mockAxios.post.mockResolvedValueOnce({ data: { url: "dummy.url" } });
+    mockAxios.post.mockResolvedValueOnce({ data: { url: "xyz.url" } });
+    const wrapper = mount(UploaderFile, {
+      props: {
+        action: "test.url",
+      },
+      global: {
+        stubs: mockComponents,
+      },
+      slots: {
+        default: "<button>Custom button</button>",
+        loading: '<div class="loading">custom loading</div>',
+        uploaded: `<template #uploaded="{ uploadedData }">
+        <div class="custom-loaded">{{ uploadedData.url }}</div>
+        </template>`,
+      },
+    });
+    expect(wrapper.get("button").text()).toBe("Custom button");
+    const fileInput = wrapper.find("input").element as HTMLInputElement;
+    setInputValue(fileInput);
+
+    await wrapper.get("input").trigger("change");
+    expect(wrapper.get(".loading").text()).toBe("custom loading");
+    await flushPromises();
+    expect(wrapper.get(".custom-loaded").text()).toBe("dummy.url");
+
+    await wrapper.get("input").trigger("change");
+    expect(wrapper.get(".loading").text()).toBe("custom loading");
+    await flushPromises();
+    expect(wrapper.get(".custom-loaded").text()).toBe("xyz.url");
+  });
+  it("before upload check", async () => {
+    const callback = vi.fn();
+    mockAxios.post.mockResolvedValueOnce({ data: { url: "dummy.url" } });
+    const checkFileSize = (file: File) => {
+      if (file.size > 2) {
+        callback();
+        return false;
+      }
+      return true;
+    };
+    const wrapper = shallowMount(UploaderFile, {
+      props: {
+        action: "test.url",
+        beforeUpload: checkFileSize,
+      },
+    });
+    const fileInput = wrapper.get("input").element as HTMLInputElement;
+    setInputValue(fileInput);
+    await wrapper.get("input").trigger("change");
+    expect(mockAxios.post).not.toHaveBeenCalled();
+    expect(wrapper.findAll("li").length).toBe(0);
+    expect(callback).toHaveBeenCalled();
+  });
+  it("before upload check using Promise", async () => {
+    mockAxios.post.mockResolvedValueOnce({ data: { url: "dummy.url" } });
+    const failedPromise = (file: File) => {
+      return Promise.reject("wrong type");
+    };
+    const successPromise = (file: File) => {
+      const newFile = new File([file], "new_name.docx", { type: file.type });
+      return Promise.resolve(newFile);
+    };
+    const successPromiseWithWrongType = () => {
+      return Promise.resolve("abcd");
+    };
+    const wrapper = shallowMount(UploaderFile, {
+      props: {
+        action: "test.url",
+        beforeUpload: failedPromise,
+      },
+    });
+    const fileInput = wrapper.get("input").element as HTMLInputElement;
+    setInputValue(fileInput);
+    await wrapper.get("input").trigger("change");
+    await flushPromises();
+    expect(mockAxios.post).not.toHaveBeenCalled();
+    expect(wrapper.findAll("li").length).toBe(0);
+    // success promise with wrong file
+    await wrapper.setProps({ beforeUpload: successPromiseWithWrongType });
+    await wrapper.get("input").trigger("change");
+    await flushPromises();
+    expect(mockAxios.post).not.toHaveBeenCalled();
+
+    // success promise with file
+    await wrapper.setProps({ beforeUpload: successPromise });
+    await wrapper.get("input").trigger("change");
+    await flushPromises();
+    const firstItem = wrapper.get("li:first-child");
+    expect(firstItem.classes()).toContain("upload-success");
+    expect(firstItem.get(".filename").text()).toBe("new_name.docx");
+  });
+  afterEach(() => {
+    mockAxios.post.mockReset();
   });
 });
